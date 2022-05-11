dc4a74111fdabcea2e90df790b3dde99b7d44d42
downy <TeamBehindDowny@gmail.com>
2022-05-11 22:46:02 +0900

    modify description for test

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const nftName = "K-Birdz Egg";
-const description = "K-Birdz, Flappy Bird Style P2E game with K-culture made by youtuber JoCoding";
-const hiddenImgUrl = "ipfs://QmbbfqR9EHdEY2Jy2dJYx8jjFftoaMnD6vgKJLMFXemVdL/hidden.mp4";
-const totalNum = 5;
+const nftName = "jogakbo"; // name of nft
+const description = "test for jogakbo"; // description of nft
+const hiddenImgUrl =
+  "ipfs://QmZZycTQTcHiwcvhD5m6ziYtnJ6MrVdM8WCNeEqoPMjZ3a/downyDog.png";
+const totalNum = 5; // amount of nfts
 
 try {
-    for (let i = 1; i <= totalNum; i++) {
-        let json = `{"name":"${nftName} #${i}","description":"${description}","image":"${hiddenImgUrl}","attributes":[{"trait_type": "Unknown","value": "Unknown"}]}`;
-        let fs = require("fs");
-        fs.writeFile(`json/${i}.json`, json, "utf8", (e)=>(e));
-    }
-    console.log("complete!");
+  for (let i = 1; i <= totalNum; i++) {
+    let json = `{"name":"${nftName} #${i}","description":"${description}","image":"${hiddenImgUrl}","attributes":[{"trait_type": "Unknown","value": "Unknown"}]}`;
+    let fs = require("fs");
+    fs.writeFile(`json/${i}.json`, json, "utf8", (e) => e);
+  }
+  console.log("complete!");
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
